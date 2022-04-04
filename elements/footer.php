0d243365cb6c320a43b158333e7eb2d9029bf35d
umair <umairmithawala007@gmail.com>
2022-04-04 15:55:29 +0530

    init commit

---
+++ elements/footer.php
@@ -0,0 +1,87 @@
+<footer class="theme-dark-bg2">
+        <div class="tp-footer-area-two pt-160 pb-70">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="tp-footer-widget footer-col-1 mb-30 wow fadeInUp" data-wow-delay=".3s">
+                            <div class="tp-footer-info tp-footer-info-three">
+                                <div class="tp-footer-info-logo mb-35">
+                                    <a href="index.html"><img src="assets/img/logo/logo-white.png" class="img-fluid" alt="img not found"></a>
+                                </div>
+                                <p class="text-white mb-35">Pulvinar seneius morbi to quisque nunc a towa faucibus netus a mone lestie nice disease</p>
+                                <div class="tp-footer-info-social tp-footer-info-social-three">
+                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                    <a href="#"><i class="fab fa-google"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="tp-footer-widget footer-col-2 mb-30 wow fadeInUp" data-wow-delay=".6s">
+                            <h4 class="tp-footer-widget-title mb-35">Our Services</h4>
+                            <ul>
+                                <li><a href="#">Carpet Cleaning</a></li>
+                                <li><a href="#">Plumbing Serivices</a></li>
+                                <li><a href="#">Park Cleaning</a></li>
+                                <li><a href="#">Residential Services</a></li>
+                                <li><a href="#">Toilet Cleaning</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="tp-footer-widget mb-30 wow fadeInUp" data-wow-delay=".9s">
+                            <h4 class="tp-footer-widget-title mb-35">Recent News</h4>
+                            <div class="tp-footer-news tp-footer-news-three">
+                                <div class="tp-footer-news-single tp-footer-news-three-single mb-10">
+                                    <div class="tp-footer-news-three-single-img">
+                                        <a href="blog-details.html"><img src="assets/img/footer/footer-blog-1.png" class="img-fluid" alt="img-not-found"></a>
+                                    </div>
+                                    <div class="tp-footer-news-three-single-text">
+                                        <h6><a href="blog-details.html">Feugiat pharetra a ductor nonuy vehicula</a></h6>
+                                        <span>Jun 02, 2021</span>
+                                    </div>
+                                </div>
+                                <div class="tp-footer-news-single tp-footer-news-three-single mb-10">
+                                    <div class="tp-footer-news-three-single-img">
+                                        <a href="blog-details.html"><img src="assets/img/footer/footer-blog-2.png" class="img-fluid" alt="img-not-found"></a>
+                                    </div>
+                                    <div class="tp-footer-news-three-single-text">
+                                        <h6><a href="blog-details.html">Curabitur tortor venenatis an scelerisque</a></h6>
+                                        <span>Jun 03, 2021</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="tp-footer-widget footer-col-4 mb-30 wow fadeInUp" data-wow-delay="1.2s">
+                            <h4 class="tp-footer-widget-title mb-40">Newsletter</h4>
+                            <div class="tp-footer-newsletter-three">
+                                <p class="mb-20">Sign up to get new exclusive offers</p>
+                                <form action="#">
+                                    <div class="tp-footer-newsletter-three-field position-relative mb-20">
+                                        <input type="email" placeholder="Enter Email">
+                                        <i class="fal fa-paper-plane"></i>
+                                    </div>
+                                    <button type="submit" class="yellow-btn"><i class="flaticon-enter"></i> Subscribe</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tp-copyright-area-two theme-dark-bg3 z-index pt-30 pb-30">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="tp-copyright tp-copyright-two text-center">
+                            <p class="m-0">Copyright ©2021 <span>Theme_pure</span>. All Rights Reserved Copyright</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ elements/header.php
@@ -0,0 +1,44 @@
+<header>
+        <div class="tp-header-area-three header-sticky">
+            <div class="tp-custom-container">
+                <div class="row justify-content-xl-center align-items-center">
+                    <div class="col-xxl-2 col-xl-3 col-8">
+                        <div class="tp-header-logo-three">
+                            <a href="index.php"><img src="assets/img/logo/tv-repair-logo.png" class="img-fluid" alt="logo not found"></a>
+                        </div>
+                    </div>
+                    <div class="col-xxl-6 col-xl-6 col-4">
+                        <div class="tp-main-menu tp-main-menu-three">
+                            <nav id="tp-mobile-menu">
+                                <ul>
+                                    <li class="menu-item-has-children"><a href="index.php">Home</a>
+                                    </li>
+                                    <li class="menu-item-has-children"><a href="service.php">Services</a>
+                                    </li>
+                                    <li class="menu-item-has-children"><a href="about.php">About Us</a>
+                                    </li>
+                                    <li><a href="contact.html">Contact Us</a></li>
+                                    <li class="menu-item-has-children"><a href="blog.php">Blog</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <!-- mobile menu activation -->
+                        <div class="side-menu-icon d-xl-none text-end">
+                            <button class="side-toggle"><i class="far fa-bars"></i></button>
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-xl-3 d-none d-xl-block">
+                        <div class="tp-header-right-three">
+                            <div class="tp-header-number-three">
+                                <span>Call Us : <a href="tel:+919036211916">+919036211916</a></span>
+                            </div>
+                            <div class="tp-header-btn-three">
+                                <a href="contact.php" class="yellow-btn"><i class="flaticon-enter"></i> Free Quote</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
